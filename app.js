66154379f178881715aa2c9ff99f2a5d370ddabe
Kwabena Anane Kwarteng <kwabena.ananeK@gmail.com>
2025-02-10 11:28:54 +0100

    Finalize user class with ubique ID

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-import { User } from './user.js'
+import { User } from './user.js';
 
-const form = document.querySelector('form')
+const form = document.querySelector('form');
 
-const initApp = () => {}
+const initApp = () => {};
 
 const handleSubmit = (e) => {
-  e.preventDefault()
-  console.log('Send')
+  e.preventDefault();
+  console.log('Send');
 
-  const data = new FormData(form) //creating a form
-  const email = data.get('email')
-  const caption = data.get('caption')
-  const description = data.get('description')
+  const data = new FormData(form); //creating a form
+  const email = data.get('email');
+  const caption = data.get('caption');
+  const description = data.get('description');
 
-  const user = new User(email, caption, description) //creating a new user instance
+  const user = new User(email, caption, description); //creating a new user instance
 
-  console.log(email, caption, description)
-}
+  console.log(email, caption, description);
+};
 
-document.addEventListener('DOMContentLoaded', initApp)
-form.addEventListener('submit', handleSubmit)
+document.addEventListener('DOMContentLoaded', initApp);
+form.addEventListener('submit', handleSubmit);
--- user.js
+++ user.js
@@ -1,9 +1,11 @@
+
+
 export class User {
   constructor(email, caption, description) {
-    this.email = email
-    this.caption = caption
-    this.description = description
-    this.issueDate = Date.now()
-    this.issueNumber = 1
+    this.email = email;
+    this.caption = caption;
+    this.description = description;
+    this.issueDate = Date.now().toLocaleString('sv-SE');
+    this.issueNumber = crypto.randomUUID().replaceAll('-', '');//generate random unique identifier
   }
 }
